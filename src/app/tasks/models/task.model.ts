c73ee40b404d2bc1d6d60340c076c9305eddd21f
Haakon Hestness <49789071+haakon-io@users.noreply.github.com>
2023-04-25 13:30:52 -0600

    Modified styles

--- src/app/tasks/models/task.model.ts
+++ src/app/tasks/models/task.model.ts
@@ -1,28 +1,3 @@
-// export class TaskModel {
-//   id: number;
-//   taskListId: string;
-//   name: string;
-//   description: string;
-//   completed: boolean;
-//   timeLength: number;
-
-//   constructor(
-//     id: number,
-//     taskListId: string,
-//     name: string,
-//     description: string,
-//     completed: boolean,
-//     timeLength: number
-//   ) {
-//     this.id = 0;
-//     this.taskListId = taskListId;
-//     this.name = name;
-//     this.description = description;
-//     this.completed = completed;
-//     this.timeLength = timeLength;
-//   }
-// }
-
 export class TaskModel {
   id: number;
   taskListId: string;
