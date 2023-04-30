88236dbf31f5840c0372e118f20471cd33166e04
Haakon Hestness <49789071+haakon-io@users.noreply.github.com>
2023-04-30 08:40:12 -0600

    Updated Task display styles

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,15 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'task-app',
+  appId: 'io.haakon.oppgaver',
+  appName: 'Oppgaver',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert'],
+    },
+  },
 };
 
 export default config;
